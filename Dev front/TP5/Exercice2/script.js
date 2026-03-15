e5b528a6b43f3386c370eae0b7f6e019248075bf
Adam-Btb <adam.bettayeb26@gmail.com>
2026-03-15 21:47:08 +0100

    Delete Dev front directory

--- Dev front/TP5/Exercice2/script.js
+++
@@ -1,5 +0,0 @@
-window.addEventListener('scroll', () => {
-    const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
-    const ratio = scrollTop / (scrollHeight - clientHeight);
-    document.body.style.backgroundColor = `rgb(${ratio * 255}, 0, 0)`;
-});
